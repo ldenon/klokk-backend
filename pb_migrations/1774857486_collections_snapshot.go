ecaff493cc41f24ce04ff3a2db4d9bc26d3bf8a4
Lucas Denon <contactlokdien@gmail.com>
2026-03-30 09:58:49 +0200

    feat: tasks

--- pb_migrations/1774857486_collections_snapshot.go
+++ pb_migrations/1774857486_collections_snapshot.go
@@ -1230,6 +1230,103 @@ func init() {
 				"type": "base",
 				"updateRule": null,
 				"viewRule": null
+			},
+			{
+				"createRule": "@request.auth.id != \"\" && @request.auth.id = project.owner",
+				"deleteRule": "@request.auth.id != \"\" && @request.auth.id = project.owner",
+				"fields": [
+					{
+						"autogeneratePattern": "[a-z0-9]{15}",
+						"hidden": false,
+						"id": "text3208210256",
+						"max": 15,
+						"min": 15,
+						"name": "id",
+						"pattern": "^[a-z0-9]+$",
+						"presentable": false,
+						"primaryKey": true,
+						"required": true,
+						"system": true,
+						"type": "text"
+					},
+					{
+						"cascadeDelete": false,
+						"collectionId": "_pb_users_auth_",
+						"hidden": false,
+						"id": "relation2375276105",
+						"maxSelect": 1,
+						"minSelect": 0,
+						"name": "user",
+						"presentable": false,
+						"required": true,
+						"system": false,
+						"type": "relation"
+					},
+					{
+						"autogeneratePattern": "",
+						"hidden": false,
+						"id": "text1579384326",
+						"max": 0,
+						"min": 0,
+						"name": "name",
+						"pattern": "",
+						"presentable": false,
+						"primaryKey": false,
+						"required": false,
+						"system": false,
+						"type": "text"
+					},
+					{
+						"hidden": false,
+						"id": "bool271442091",
+						"name": "done",
+						"presentable": false,
+						"required": false,
+						"system": false,
+						"type": "bool"
+					},
+					{
+						"cascadeDelete": false,
+						"collectionId": "pbc_484305853",
+						"hidden": false,
+						"id": "relation800313582",
+						"maxSelect": 1,
+						"minSelect": 0,
+						"name": "project",
+						"presentable": false,
+						"required": true,
+						"system": false,
+						"type": "relation"
+					},
+					{
+						"hidden": false,
+						"id": "autodate2990389176",
+						"name": "created",
+						"onCreate": true,
+						"onUpdate": false,
+						"presentable": false,
+						"system": false,
+						"type": "autodate"
+					},
+					{
+						"hidden": false,
+						"id": "autodate3332085495",
+						"name": "updated",
+						"onCreate": true,
+						"onUpdate": true,
+						"presentable": false,
+						"system": false,
+						"type": "autodate"
+					}
+				],
+				"id": "pbc_2602490748",
+				"indexes": [],
+				"listRule": "@request.auth.id != \"\" && @request.auth.id = user",
+				"name": "tasks",
+				"system": false,
+				"type": "base",
+				"updateRule": "@request.auth.id != \"\" && @request.auth.id = project.owner",
+				"viewRule": null
 			}
 		]`
 
