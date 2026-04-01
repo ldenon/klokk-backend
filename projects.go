0be7f3dbbd6f7f731693613aa4015cb0835735b0
Lucas Denon <contactlokdien@gmail.com>
2026-04-01 15:00:54 +0200

    feat: create project on user signup

--- projects.go
+++ projects.go
@@ -1,7 +1,30 @@
 package main
 
-import "github.com/pocketbase/pocketbase"
+import (
+	"github.com/pocketbase/pocketbase"
+	"github.com/pocketbase/pocketbase/core"
+)
 
 func registerProjectHooks(app *pocketbase.PocketBase) {
-	// Project-specific hooks can be registered here.
+	app.OnRecordAfterCreateSuccess("users").BindFunc(func(e *core.RecordEvent) error {
+		collection, err := app.FindCollectionByNameOrId("projects")
+		if err != nil {
+			return err
+		}
+
+		project := core.NewRecord(collection)
+		project.Set("name", "Personal")
+		project.Set("owner", e.Record.Id)
+
+		if err := app.Save(project); err != nil {
+			return err
+		}
+
+		e.Record.Set("currentProject", project.Id)
+		if err := app.Save(e.Record); err != nil {
+			return err
+		}
+
+		return e.Next()
+	})
 }
